26abf49f814a4edc553e1298e602da6220b9ab7f
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-20 17:00:54 -0500

    retrieving pokemon names

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
+import axios from 'axios';
 import './App.css';
+const Request = require('request');
 
 class App extends React.Component{
+constructor(props){
+  super(props);
+  axios.get('https://pokeapi.co/api/v2/generation/1')
+  .then(response => { 
+    const poke = response.data;
+     console.log(poke.pokemon_species); 
+    })
+  
+  this.state={
+    text: ''
+  }
+}
+
+
+
   render(){
     return (
       <div className="App">
         <Grid container spacing={3} className='container'>
           
-          <Grid item justify='center' xs={4} sm={4} md={4}>
+          <Grid item align='center' xs={4} sm={4} md={4}>
           <TextField variant='outlined' placeholder='search by #' />
           </Grid>
           <Grid item align='center' xs={4} sm={4} md={4}>
