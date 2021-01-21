915e5e8ba801685afb0f31df46539fba114c4abf
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-21 10:45:10 -0500

    Added updates

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import axios from "axios";
+import PokemonData from "./pokenames";
 import "./App.css";
 
 
@@ -16,27 +17,32 @@ class App extends React.Component {
       },
     };
   
-  }
-
-  render() {
     axios.get("https://pokeapi.co/api/v2/generation/1")
     .then((response) => {
     let pokemonNames = response.data;
-    this.setState({rawData: pokemonNames.pokemon_species});
+    this.setState({rawData: pokemonNames.pokemon_species[0]['name']});
+    console.log(this.state.rawData);
     });
-    
+  }
+
+  render() {
+    // axios.get("https://pokeapi.co/api/v2/generation/1")
+    // .then((response) => {
+    // let pokemonNames = response.data;
+    // this.setState({rawData: pokemonNames.pokemon_species});
+ 
     return (
       <div className="App">
         <Grid container spacing={3} className="container">
           <form> 
-          <Grid item align="center" xs={4} sm={4} md={4}>
-            <TextField variant="outlined" placeholder="search by #" />
+          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+            <TextField fullWidth variant="outlined" placeholder="search by #" />
           </Grid>
-          <Grid item align="center" xs={4} sm={4} md={4}>
-            <TextField variant="outlined" placeholder="search by name" />
+          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+            <TextField fullWidth variant="outlined" placeholder="search by name" />
           </Grid>
-          <Grid item align="center" xs={4} sm={4} md={4}>
-            <TextField variant="outlined" placeholder="search by type" />
+          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+            <TextField fullWidth variant="outlined" placeholder="search by type" />
           </Grid>
           </form>
         </Grid>
