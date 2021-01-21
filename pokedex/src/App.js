cfb1fe60744096f553c0f97991742181c92937b8
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-21 15:03:01 -0500

    getpokeNames and updateform function v1

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,48 +2,77 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import axios from "axios";
-import PokemonData from "./pokenames";
+import pokemonPage from "./pokemonPage";
+import Button from "@material-ui/core/Button";
 import "./App.css";
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rawData: '',
+      rawData: "",
       pokemonInfo: {
         name: "",
         data: "",
+        error: ""
       },
     };
-  
-    axios.get("https://pokeapi.co/api/v2/generation/1")
-    .then((response) => {
-    let pokemonNames = response.data;
-    this.setState({rawData: pokemonNames.pokemon_species[0]['name']});
+  }
+  getPokemonNames(event) {
+    event.preventDefault();
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokemonInfo.name}`)
+      .then((response) => {
+        let pokemonNames = response.data;
+        this.setState({ rawData: pokemonNames });
+      });
     console.log(this.state.rawData);
-    });
   }
 
+  updateForm(which, event) {
+    this.setState({
+      [which]: {
+        name: event.target.value,
+        error: this.state[which].error,
+      },
+    });
+  }
   render() {
-    // axios.get("https://pokeapi.co/api/v2/generation/1")
-    // .then((response) => {
-    // let pokemonNames = response.data;
-    // this.setState({rawData: pokemonNames.pokemon_species});
- 
     return (
       <div className="App">
         <Grid container spacing={3} className="container">
-          <form> 
-          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
-            <TextField fullWidth variant="outlined" placeholder="search by #" />
-          </Grid>
-          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
-            <TextField fullWidth variant="outlined" placeholder="search by name" />
-          </Grid>
-          <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
-            <TextField fullWidth variant="outlined" placeholder="search by type" />
-          </Grid>
+          <form
+            onSubmit={(event) => {
+              this.getPokemonNames(event);
+            }}
+          >
+            <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+              <TextField
+                fullWidth
+                variant="outlined"
+                placeholder="search by #"
+              />
+            </Grid>
+            <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+              <TextField
+                fullWidth
+                variant="outlined"
+                placeholder="search by name"
+                value={this.state.pokemonInfo.name}
+                error={this.state.pokemonInfo.error}
+                onChange={(event) => {
+                  this.updateForm('pokemonInfo.name',event);
+                }}
+              />
+            </Grid>
+            <Grid item align="center" xs={4} sm={4} md={4} lg={4} xl={4}>
+              <TextField
+                fullWidth
+                variant="outlined"
+                placeholder="search by type"
+              />
+            </Grid>
+            <Button></Button>
           </form>
         </Grid>
       </div>
