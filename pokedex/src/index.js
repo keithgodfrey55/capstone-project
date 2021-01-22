00a3bb4d146af9a9eb141a8886d72f680d5c3ae6
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-22 16:02:47 -0500

    Starting to creat initial files 2

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -9,12 +9,14 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <div>
       <Grid container>
         <Grid item xs={12} align="center">
           <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/62902855-31e8-48de-986e-5080e8ef5f15/d5uxsvu-cbf56dfe-0c82-40f9-928b-1e756acf0236.png/v1/fill/w_312,h_112,strp/pokedex_vector_logo_by_macoscrazy_d5uxsvu-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMTIiLCJwYXRoIjoiXC9mXC82MjkwMjg1NS0zMWU4LTQ4ZGUtOTg2ZS01MDgwZThlZjVmMTVcL2Q1dXhzdnUtY2JmNTZkZmUtMGM4Mi00MGY5LTkyOGItMWU3NTZhY2YwMjM2LnBuZyIsIndpZHRoIjoiPD0zMTIifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.daudD2Eg6CKty-X3Ecc7TohhgtPMxWj0KkqSTBSLvAc" />
         </Grid>
       </Grid>
       <App />
+      </div>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
